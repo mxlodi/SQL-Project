04d5cb206702a6fba0b60e45b2258abc894380c6
lafange <raksasparkle@gmail.com>
2024-08-19 19:32:06 +0700

    second initial

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function HomePage() {
-  return <div>Home Page</div>;
+  // INITIAL
+  const [posts, setPosts] = React.useState([]);
+
+  // GET POSTS
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/posts");
+        setPosts(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchUser();
+  }, [])
+
+
+
 }
 
 export default HomePage;
